5869b988312198827365b6823b348f39766e67e9
suryan0008 <158776889+suryan0008@users.noreply.github.com>
2024-02-04 09:51:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.j
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "@adityasuryan")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "@adityasuryan")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5090817443").split()))
 
